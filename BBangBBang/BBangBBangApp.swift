9447ba00c46ccd52c7afe49ced01d47a903f09d5
DON <booby9709@gmail.com>
2025-06-25 11:37:04 +0900

    git repository create

---
+++ BBangBBang/BBangBBangApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBangBBangApp.swift
+//  BBangBBang
+//
+//  Created by 이돈혁 on 6/25/25.
+//
+
+import SwiftUI
+
+@main
+struct BBangBBangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
